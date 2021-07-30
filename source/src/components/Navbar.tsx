9922ff1fa4fbfe3bab26d59b1364b259099724a8
developomp <developomp@gmail.com>
2021-07-30 15:13:11 +0900

    replaced github button with search button

--- source/src/components/Navbar.tsx
+++ source/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faSearch } from "@fortawesome/free-solid-svg-icons"
 import styled from "styled-components"
 import ReactTooltip from "react-tooltip"
 import { Link } from "react-router-dom"
@@ -11,7 +11,6 @@ import NavbarData from "../data/NavbarData"
 
 import Sidebar from "./Sidebar"
 import ThemeToggleButton from "./ThemeToggleButton"
-// import { faSearch } from "@fortawesome/free-solid-svg-icons"
 
 const StyledNav = styled.nav`
 	position: absolute;
@@ -62,10 +61,6 @@ const StyledLink = styled(Link)`
 	${theming.styles.navbarButtonStyle}
 `
 
-const StyledALink = styled.a`
-	${theming.styles.navbarButtonStyle}
-`
-
 export default class Navbar extends React.Component {
 	render() {
 		return (
@@ -88,21 +83,7 @@ export default class Navbar extends React.Component {
 
 					<ThemeToggleButton />
 
-					<StyledALink
-						data-tip
-						data-for="github"
-						href="https://github.com/developomp/developomp-site"
-						target="_blank"
-					>
-						<FontAwesomeIcon icon={faGithub} />
-					</StyledALink>
-					{!isMobile && (
-						<ReactTooltip id="github" type="dark" effect="solid">
-							<span>View source code</span>
-						</ReactTooltip>
-					)}
-
-					{/* <StyledLink
+					<StyledLink
 						data-tip
 						data-for="search"
 						to={`${process.env.PUBLIC_URL}/search`}
@@ -113,7 +94,7 @@ export default class Navbar extends React.Component {
 						<ReactTooltip id="search" type="dark" effect="solid">
 							<span>Search</span>
 						</ReactTooltip>
-					)} */}
+					)}
 
 					<Sidebar />
 				</StyledContainer>
