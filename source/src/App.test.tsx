0527f6b924a1de89cc400059b1828e4b05887ea4
developomp <developomp@gmail.com>
2021-08-02 07:33:31 +0900

    removed testing and bumped dependencies

--- source/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from "@testing-library/react"
-import App from "./App"
-
-test("renders content", () => {
-	const { getByTestId } = render(<App />)
-	const content = getByTestId("content")
-	expect(content).toBeInTheDocument()
-})
