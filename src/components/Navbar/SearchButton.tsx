51dbf855c2eea21a42afc3d9565a9c123d314a87
developomp <developomp@gmail.com>
2022-03-26 18:58:11 +0900

    added localization to search button

--- src/components/Navbar/SearchButton.tsx
+++ src/components/Navbar/SearchButton.tsx
@@ -1,3 +1,4 @@
+import { useContext } from "react"
 import { Link } from "react-router-dom"
 import ReactTooltip from "react-tooltip"
 
@@ -6,7 +7,11 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons"
 
 import { StyledLink } from "./Navbar"
 
+import { globalContext } from "../../globalContext"
+
 const SearchButton = () => {
+	const { globalState } = useContext(globalContext)
+
 	return (
 		<>
 			<div>
@@ -21,7 +26,7 @@ const SearchButton = () => {
 				</Link>
 			</div>
 			<ReactTooltip id="search" type="dark" effect="solid">
-				<span>Search</span>
+				<span>{globalState.locale == "en" ? "Search" : "검색"}</span>
 			</ReactTooltip>
 		</>
 	)
