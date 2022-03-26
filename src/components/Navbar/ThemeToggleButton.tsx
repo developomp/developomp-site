e64f26e9275ead5b5363f409956b78d86956fcf2
developomp <developomp@gmail.com>
2022-03-26 18:54:34 +0900

    added basic localization to theme toggle button

--- src/components/Navbar/ThemeToggleButton.tsx
+++ src/components/Navbar/ThemeToggleButton.tsx
@@ -1,4 +1,5 @@
-import styled, { ThemeConsumer } from "styled-components"
+import { useContext } from "react"
+import styled from "styled-components"
 import { isMobile } from "react-device-detect"
 import ReactTooltip from "react-tooltip"
 
@@ -6,6 +7,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons"
 
 import theming from "../../styles/theming"
+import { ActionsEnum, globalContext } from "../../globalContext"
 
 const StyledThemeButton = styled.div`
 	${theming.styles.navbarButtonStyle}
@@ -17,26 +19,35 @@ const StyledThemeButton = styled.div`
 `
 
 const ThemeToggleButton = () => {
+	const { globalState, dispatch } = useContext(globalContext)
+	const theme = globalState.theme
+
 	return (
-		<ThemeConsumer>
-			{({ currentTheme, setTheme }) => (
-				<>
-					<StyledThemeButton
-						data-tip
-						data-for="theme"
-						onClick={() => setTheme(currentTheme === "dark" ? "light" : "dark")}
-					>
-						{currentTheme == "dark" && <FontAwesomeIcon icon={faMoon} />}
-						{currentTheme == "light" && <FontAwesomeIcon icon={faSun} />}
-					</StyledThemeButton>
-					{!isMobile && (
-						<ReactTooltip id="theme" type="dark" effect="solid">
-							<span>Using {currentTheme} theme</span>
-						</ReactTooltip>
+		<>
+			<StyledThemeButton
+				data-tip
+				data-for="theme"
+				onClick={() =>
+					dispatch({
+						type: ActionsEnum.UPDATE_THEME,
+						payload: theme === "dark" ? "light" : "dark",
+					})
+				}
+			>
+				{theme == "dark" && <FontAwesomeIcon icon={faMoon} />}
+				{theme == "light" && <FontAwesomeIcon icon={faSun} />}
+			</StyledThemeButton>
+
+			{!isMobile && (
+				<ReactTooltip id="theme" type="dark" effect="solid">
+					{globalState.locale == "en" ? (
+						<span>Using {theme} theme</span>
+					) : (
+						<span>{theme == "dark" ? "어두운" : "밝은"} 테마 사용중</span>
 					)}
-				</>
+				</ReactTooltip>
 			)}
-		</ThemeConsumer>
+		</>
 	)
 }
 
