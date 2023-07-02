002fb4bea7361cb14a5928e465d46d8c3c40ff17
developomp <developomp@gmail.com>
2023-07-02 10:56:12 +0900

    refactor: tidy up tailwind configs
    - add tailwind to all apps
    - share tailwind themes and stuff

---
+++ apps/main/tailwind.config.js
@@ -0,0 +1,5 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    presets: [require("@developomp-site/tailwind-config/tailwind.config.js")],
+    content: ["./src/**/*.{html,js,svelte,ts}"],
+}
