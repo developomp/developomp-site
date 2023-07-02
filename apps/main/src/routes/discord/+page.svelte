5cfc8adbffba126b1d6495cec2a5217deadb707c
developomp <developomp@gmail.com>
2023-07-02 15:12:15 +0900

    fix(main): fix site not building

--- apps/main/src/routes/discord/+page.svelte
+++ apps/main/src/routes/discord/+page.svelte
@@ -1,10 +1,14 @@
 <script>
     import Discord from "@inqling/svelte-icons/simple-icons/discord.svelte"
 
+    import { browser } from "$app/environment"
+
     import { discordInviteLink } from "../../constants"
 
     // redirect to developomp's discord server
-    window.location.replace(discordInviteLink)
+    if (browser) {
+        window.location.replace(discordInviteLink)
+    }
 </script>
 
 <div>
