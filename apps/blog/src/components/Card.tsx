696dd1a95ed00e01b16f40739f9e85f22c4742e6
developomp <developomp@gmail.com>
2023-07-09 10:55:58 +0900

    refactor(blog): widen content area (#77)

--- apps/blog/src/components/Card.tsx
+++ apps/blog/src/components/Card.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function Card({ children, className }: Props) {
     return (
         <div
-            className={`${className} flex h-fit w-full max-w-screen-mobile flex-col rounded-md bg-light-card-bg p-8 shadow-lg dark:bg-dark-card-bg`}
+            className={`${className} flex h-fit w-full max-w-screen-mobile flex-col rounded-md bg-light-card-bg px-6 py-8 shadow-lg dark:bg-dark-card-bg`}
         >
             {children}
         </div>
