6787eb1c00a50be0ee9723c58c9bb578ba287c05
developomp <developomp@gmail.com>
2023-07-09 10:48:24 +0900

    refactor(blog): improve meta tags styling (#76)

--- apps/blog/src/pages/Page/Meta.tsx
+++ apps/blog/src/pages/Page/Meta.tsx
@@ -9,37 +9,38 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export default function Meta(props: { fetchedPage: PageData }) {
     return (
-        <div className="text-light-text-gray dark:text-dark-text-gray">
+        <div className="flex flex-wrap gap-x-4 text-light-text-gray dark:text-dark-text-gray">
             {/* posts count */}
             {props.fetchedPage.length > 0 && (
-                <>
+                <div className="flex flex-nowrap items-center gap-2 whitespace-nowrap">
                     <FontAwesomeIcon icon={faFile} />
-                    &nbsp;&nbsp;
                     {props.fetchedPage.length} post
                     {props.fetchedPage.length > 1 && "s"}{" "}
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                </>
+                </div>
             )}
             {/* date */}
-            <FontAwesomeIcon icon={faCalendar} />
-            &nbsp;&nbsp;
-            {props.fetchedPage.date || "Unknown date"}
-            &nbsp;&nbsp;&nbsp;&nbsp;
+            <div className="flex flex-nowrap items-center gap-2 whitespace-nowrap">
+                <FontAwesomeIcon icon={faCalendar} />
+                {props.fetchedPage.date || "Unknown date"}
+            </div>
+
             {/* read time */}
-            <FontAwesomeIcon icon={faHourglass} />
-            &nbsp;&nbsp;
-            {props.fetchedPage.readTime
-                ? props.fetchedPage.readTime + " read"
-                : "unknown length"}
-            &nbsp;&nbsp;&nbsp;&nbsp;
+            <div className="flex flex-nowrap items-center gap-2 whitespace-nowrap">
+                <FontAwesomeIcon icon={faHourglass} />
+                {props.fetchedPage.readTime
+                    ? props.fetchedPage.readTime + " read"
+                    : "unknown length"}
+            </div>
+
             {/* word count */}
-            <FontAwesomeIcon icon={faBook} />
-            &nbsp;&nbsp;
-            {props.fetchedPage.wordCount
-                ? props.fetchedPage.wordCount +
-                  " word" +
-                  (props.fetchedPage.wordCount > 1 && "s")
-                : "unknown words"}
+            <div className="flex flex-nowrap items-center gap-2 whitespace-nowrap">
+                <FontAwesomeIcon icon={faBook} />
+                {props.fetchedPage.wordCount
+                    ? props.fetchedPage.wordCount +
+                      " word" +
+                      (props.fetchedPage.wordCount > 1 && "s")
+                    : "unknown words"}
+            </div>
         </div>
     )
 }
