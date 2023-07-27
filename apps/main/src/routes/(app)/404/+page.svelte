62b34c9c483097781389f723aaac9deb12dd7e53
developomp <developomp@gmail.com>
2023-07-27 14:32:24 +0900

    feat(main): add 404 page

---
+++ apps/main/src/routes/(app)/404/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import "$/routes/(app)/app.css"
+
+    import { page } from "$app/stores"
+</script>
+
+<h1 class="mx-auto mt-10 text-center text-8xl font-normal">
+    {$page.status}
+</h1>
+<h2 class="mx-auto mt-6 text-center text-3xl font-normal">
+    {$page.error?.message}
+</h2>
