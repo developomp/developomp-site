ce81f5d59cd27e0fdebd8f601c18e3fdda337233
developomp <developomp@gmail.com>
2023-07-28 22:39:38 +0900

    chore: fix 404 page not working for main site

--- apps/main/src/routes/(app)/404/+page.svelte
+++ apps/main/src/routes/(app)/404/+page.svelte
@@ -1,12 +1,6 @@
 <script>
     import "$/routes/(app)/app.css"
-
-    import { page } from "$app/stores"
 </script>
 
-<h1 class="mx-auto mt-10 text-center text-8xl font-normal">
-    {$page.status}
-</h1>
-<h2 class="mx-auto mt-6 text-center text-3xl font-normal">
-    {$page.error?.message}
-</h2>
+<h1 class="mx-auto mt-10 text-center text-8xl font-normal">404</h1>
+<h2 class="mx-auto mt-6 text-center text-3xl font-normal">Not Found</h2>
