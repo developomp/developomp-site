5d61043ed1d2d5d61bfa1496e611bce33df471d3
developomp <developomp@gmail.com>
2023-07-29 09:17:46 +0900

    test(blog): 404 page title

--- apps/blog/e2e/title.spec.ts
+++ apps/blog/e2e/title.spec.ts
@@ -1,9 +1,14 @@
 import { expect, test } from "@playwright/test"
 
+const prefix = "pomp's blog | "
+
 test("should have proper title", async ({ page }) => {
     await page.goto("/")
-    expect(await page.title()).toEqual("pomp's blog | Home")
+    expect(await page.title()).toEqual(`${prefix}Home`)
 
     await page.goto("/posts/test-post")
-    expect(await page.title()).toEqual("pomp's blog | Test post")
+    expect(await page.title()).toEqual(`${prefix}Test post`)
+
+    await page.goto("/404")
+    expect(await page.title()).toEqual(`${prefix}Page Not Found`)
 })
