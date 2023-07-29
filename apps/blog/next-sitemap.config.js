6ba8a7f3cbdd91b3163935e9f908c93a07df25fd
developomp <developomp@gmail.com>
2023-07-29 11:48:16 +0900

    feat: auto robots.txt and sitemap gen

---
+++ apps/blog/next-sitemap.config.js
@@ -0,0 +1,8 @@
+// https://next-sitemap.iamvishnusankar.com/docs/documentation/configuration
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://blog.developomp.com",
+    generateRobotsTxt: true,
+    outDir: "build",
+    priority: 0.8,
+}
---
+++ apps/portfolio/next-sitemap.config.js
@@ -0,0 +1,8 @@
+// https://next-sitemap.iamvishnusankar.com/docs/documentation/configuration
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://portfolio.developomp.com",
+    generateRobotsTxt: true,
+    outDir: "dist",
+    priority: 0.8,
+}
