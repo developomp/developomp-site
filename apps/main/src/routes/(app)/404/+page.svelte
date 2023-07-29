810d994688bb3ebcbbd6b1061cf2106099d9bd4d
developomp <developomp@gmail.com>
2023-07-29 10:01:19 +0900

    refactor(main): improve title of 404 page

--- apps/main/src/routes/(app)/404/+page.svelte
+++ apps/main/src/routes/(app)/404/+page.svelte
@@ -2,5 +2,9 @@
     import "$/routes/(app)/app.css"
 </script>
 
+<svelte:head>
+    <title>developomp | Page Not Found</title>
+</svelte:head>
+
 <h1 class="mx-auto mt-10 text-center text-8xl font-normal">404</h1>
-<h2 class="mx-auto mt-6 text-center text-3xl font-normal">Not Found</h2>
+<h2 class="mx-auto mt-6 text-center text-3xl font-normal">Page Not Found</h2>
