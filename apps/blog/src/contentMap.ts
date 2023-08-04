cf653915cd77c6265977254ce18373c235c73edf
developomp <developomp@gmail.com>
2023-08-04 14:55:36 +0900

    refactor: use type import

--- apps/blog/src/contentMap.ts
+++ apps/blog/src/contentMap.ts
@@ -1,4 +1,4 @@
 import contentMapJson from "@developomp-site/content/dist/map.json"
-import { ContentMap } from "@developomp-site/content/src/types/types"
+import { type ContentMap } from "@developomp-site/content/src/types/types"
 
 export default contentMapJson as ContentMap
