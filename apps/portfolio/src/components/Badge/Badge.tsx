a3c0990804242022822650596e5b371f8c8b1df2
developomp <developomp@gmail.com>
2023-08-08 00:52:23 +0900

    refactor: remove unnecessary edge case handling

--- apps/portfolio/src/components/Badge/Badge.tsx
+++ apps/portfolio/src/components/Badge/Badge.tsx
@@ -6,33 +6,25 @@ interface BadgeProps {
     slug: string
 }
 
-const sharedBadgeStyles = "flex w-fit items-center px-2 py-1 text-xs"
-const sharedInnerBadgeStyles = "badge mr-1 inline-block w-6 align-middle"
-
 export default async function Badge({ slug }: BadgeProps) {
     const badgeData: BadgeType = await import(
         `@developomp-site/content/dist/icons/${slug}.json`
     )
 
     if (!badgeData)
-        return (
-            <div className={sharedBadgeStyles}>
-                <div className={sharedInnerBadgeStyles} />
-                <span>Loading...</span>
-            </div>
-        )
+        throw `"@developomp-site/content/dist/icons/${slug}.json" does not exist`
 
     return (
         <div
             style={{ backgroundColor: badgeData.hex }}
-            className={`${sharedBadgeStyles} ${
+            className={`flex w-fit items-center px-2 py-1 text-xs ${
                 badgeData.isDark
                     ? "text-dark-text-default"
                     : "text-light-text-default"
             }`}
         >
             <div
-                className={`${sharedInnerBadgeStyles} ${
+                className={`badge mr-1 inline-block w-6 align-middle ${
                     badgeData.isDark ? "dark-badge" : "light-badge"
                 }`}
                 dangerouslySetInnerHTML={{ __html: badgeData.svg }}
