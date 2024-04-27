6d362eb5c4d6ffd1be848d9f487ff11c41e98b20
developomp <developomp@protonmail.com>
2024-04-27 23:39:23 +0900

    feat(main): use vite's rewrite instead of vercel's

--- apps/main/vite.config.ts
+++ apps/main/vite.config.ts
@@ -11,5 +11,15 @@ export default defineConfig({
     },
     server: {
         open: true,
+        proxy: {
+            // https://umami.is/docs/guides/bypass-ad-blockers
+            // https://vitejs.dev/config/server-options.html#server-proxy
+            // https://developomp.com/stats/* -> https://analytics.umami.is/*
+            "^/stats/.*": {
+                target: "https://analytics.umami.is/",
+                changeOrigin: true,
+                rewrite: (path) => path.slice(6), // slice "/stats" off the string (which is 6 characters long)
+            },
+        },
     },
 })
