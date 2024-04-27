9423645ee5ca860c2df55b7566a76a8bd8be8e81
developomp <developomp@protonmail.com>
2024-04-27 23:27:10 +0900

    fix: test failing

--- apps/blog/playwright.config.ts
+++ apps/blog/playwright.config.ts
@@ -1,5 +1,23 @@
-import { createConfig } from "@developomp-site/playwright-config"
+import { defineConfig } from "@playwright/test"
 
-export default createConfig({
-    port: 3000,
+const SECOND = 1000
+const baseURL = "http://localhost:3000"
+
+// https://playwright.dev/docs/test-configuration
+export default defineConfig({
+    // Artifacts folder where screenshots, videos, and traces are stored.
+    outputDir: "test-results/",
+
+    // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
+    webServer: {
+        command: "pnpm dev:headless",
+        url: baseURL,
+        timeout: 10 * SECOND,
+    },
+
+    use: {
+        // Use baseURL so to make navigation relative.
+        // https://playwright.dev/docs/api/class-testoptions#test-options-base-url
+        baseURL,
+    },
 })
