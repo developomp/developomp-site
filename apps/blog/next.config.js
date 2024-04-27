9cc89a1a2a38303ce1df9fa113794a643c61dc0d
developomp <developomp@protonmail.com>
2024-04-27 23:34:00 +0900

    feat(blog): use nextjs rewrite instead of vercel's

--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // https://umami.is/docs/guides/bypass-ad-blockers#proxying
+    async rewrites() {
+        return [
+            {
+                source: "/stats/:match*",
+                destination: "https://analytics.umami.is/:match*",
+            },
+        ]
+    },
     experimental: {
         externalDir: true,
     },
