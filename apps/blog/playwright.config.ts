d715961fc497aea7cc588995407dc00ec5d38143
developomp <developomp@protonmail.com>
2024-04-28 00:50:49 +0900

    refactor: improve playwright config reuse

--- apps/blog/playwright.config.ts
+++ apps/blog/playwright.config.ts
@@ -1,23 +1,6 @@
-import { defineConfig } from "@playwright/test"
-
-const SECOND = 1000
-const baseURL = "http://localhost:3000"
+import { createConfig } from "@developomp-site/playwright-config"
 
 // https://playwright.dev/docs/test-configuration
-export default defineConfig({
-    // Artifacts folder where screenshots, videos, and traces are stored.
-    outputDir: "test-results/",
-
-    // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
-    webServer: {
-        command: "pnpm dev:headless",
-        url: baseURL,
-        timeout: 10 * SECOND,
-    },
-
-    use: {
-        // Use baseURL so to make navigation relative.
-        // https://playwright.dev/docs/api/class-testoptions#test-options-base-url
-        baseURL,
-    },
+export default createConfig({
+    port: 3000,
 })
