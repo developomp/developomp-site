dd1325bb846d1cb47ac02a176910157e6224c11d
developomp <developomp@protonmail.com>
2024-04-28 18:11:24 +0900

    feat(main): add sentry

--- apps/main/vite.config.ts
+++ apps/main/vite.config.ts
@@ -1,9 +1,18 @@
+import { sentrySvelteKit } from "@sentry/sveltekit"
 import { sveltekit } from "@sveltejs/kit/vite"
 import path from "path"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-    plugins: [sveltekit()],
+    plugins: [
+        sentrySvelteKit({
+            sourceMapsUploadOptions: {
+                org: "developomp",
+                project: "developomp-site-main",
+            },
+        }),
+        sveltekit(),
+    ],
     resolve: {
         alias: {
             $: path.resolve("./src"),
