8be3ae370f82540ec175a3acfa55cd9b8794bb2d
developomp <developomp@protonmail.com>
2024-09-10 10:43:58 +0900

    fix: broken analytics

--- apps/main/vite.config.ts
+++ apps/main/vite.config.ts
@@ -11,15 +11,5 @@ export default defineConfig({
     },
     server: {
         open: true,
-        proxy: {
-            // https://umami.is/docs/guides/bypass-ad-blockers
-            // https://vitejs.dev/config/server-options.html#server-proxy
-            // https://developomp.com/stats/* -> https://umami.developomp.com/*
-            "^/stats/.*": {
-                target: "https://umami.developomp.com/",
-                changeOrigin: true,
-                rewrite: (path) => path.slice(6), // slice "/stats" off the string (which is 6 characters long)
-            },
-        },
     },
 })
