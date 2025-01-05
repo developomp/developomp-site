c3ce39984982cc63a67f8ee9b6e64af23890f9ce
developomp <developomp@protonmail.com>
2025-01-05 10:05:12 +0900

    fix: vercel not being able to use require in module

--- apps/blog/tailwind.config.js
+++ apps/blog/tailwind.config.js
@@ -1,6 +1,8 @@
+import myPreset from "@developomp-site/tailwind-config/tailwind.config.js"
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    presets: [require("@developomp-site/tailwind-config/tailwind.config.js")],
+    presets: [myPreset],
     content: [
         "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
