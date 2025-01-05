187d470ba6ecd9afb43295035c7eba61ecc498b3
developomp <developomp@protonmail.com>
2025-01-05 09:01:40 +0900

    ci: use devenv for testing

--- devenv.nix
+++ devenv.nix
@@ -16,6 +16,11 @@
     PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = true;
   };
 
+  enterTest = ''
+    pnpm test:unit
+    pnpm test:e2e
+  '';
+
   languages = {
     # https://devenv.sh/supported-languages/javascript
     javascript = {
