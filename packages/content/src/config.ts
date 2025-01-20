5dc2f2d69e4e18ce4ce60851476355d37822c1ee
developomp <developomp@protonmail.com>
2025-01-20 18:51:46 +0900

    refactor: remove unused variable

--- packages/content/src/config.ts
+++ packages/content/src/config.ts
@@ -2,6 +2,5 @@ export const markdownPath = "./markdown" // where it will look for markdown docu
 export const outPath = "./dist" // path to the json database
 
 export const contentDirectoryPath = `${outPath}/content`
-export const iconsDirectoryPath = `${outPath}/icons`
 export const mapFilePath = `${outPath}/map.json`
 export const searchIndexFilePath = `${outPath}/search.json`
